bc2239a02dd0cec4db9d671490873d803d2808f7
rachellefl <rachelle@frontlinec.com>
2023-02-20 11:50:55 +0800

    change hash route

---
+++ precache-manifest.3872d4613c0d3f5e11953db1c428a2ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4475db314ef27594de4c0ab0f0a3beee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "126516528ffb82a6ed7e",
+    "url": "/static/css/main.036d897a.chunk.css"
+  },
+  {
+    "revision": "5b7efe9f2c2eb398ff9f",
+    "url": "/static/js/2.ecca2305.chunk.js"
+  },
+  {
+    "revision": "8ded5c4bdf4c7de2eb4864a500eacf42",
+    "url": "/static/js/2.ecca2305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126516528ffb82a6ed7e",
+    "url": "/static/js/main.7d7e26c3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7d7e26c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ca1aba769cd7b61b53",
+    "url": "/static/js/runtime-main.75561206.js"
+  },
+  {
+    "revision": "458142f4882510a687dcb5586ed4bd94",
+    "url": "/static/media/logo.458142f4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca8f7ac878eb6dfa09444ad801a50e17.js"
+  "/precache-manifest.3872d4613c0d3f5e11953db1c428a2ec.js"
 );
 
 self.addEventListener('message', (event) => {
