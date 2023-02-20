e983aa17dcef5b2c3beacaa19cfc148f39848303
rachellefl <rachelle@frontlinec.com>
2023-02-20 10:49:58 +0800

    push build test

---
+++ build/precache-manifest.c3716e6b821df0b143d8b1eaed6057d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c00fdd565d47a2f201f29f05295a74e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a7e22f6008cd7a85458",
+    "url": "/static/css/main.036d897a.chunk.css"
+  },
+  {
+    "revision": "ada14f2e9c6d205d327a",
+    "url": "/static/js/2.5cfe6ccb.chunk.js"
+  },
+  {
+    "revision": "8ded5c4bdf4c7de2eb4864a500eacf42",
+    "url": "/static/js/2.5cfe6ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7e22f6008cd7a85458",
+    "url": "/static/js/main.0a4f0505.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0a4f0505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ca1aba769cd7b61b53",
+    "url": "/static/js/runtime-main.75561206.js"
+  },
+  {
+    "revision": "458142f4882510a687dcb5586ed4bd94",
+    "url": "/static/media/logo.458142f4.png"
+  }
+]);
